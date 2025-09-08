317524dcb2999eb53de4317be78dfa9fa35830a2
carlosribeiro <carlos.a.o.r@icloud.com>
2025-09-08 07:39:27 -0500

    feat: implement weather flow with MVVM (model, repository, viewmodel, activity)

--- app/src/main/java/ui/view/MainActivity.kt
+++ app/src/main/java/ui/view/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.carlosribeiro.tempo_wt
+package ui.view
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.carlosribeiro.tempo_wt.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
